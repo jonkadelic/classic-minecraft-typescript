03ccd617c65329e41ef2a15d80b8fe22d5cd84ad
Strultz <69481716+Strultz@users.noreply.github.com>
2023-06-16 02:48:48 -0500

    mfw

--- src/com/mojang/minecraft/gui/BlockSelectScreen.ts
+++ src/com/mojang/minecraft/gui/BlockSelectScreen.ts
@@ -20,9 +20,9 @@ export class BlockSelectScreen extends GuiScreen {
         let block = this.getBlockOnScreen(mx, my)
         BlockSelectScreen.fillGradient(buffer, Math.trunc(this.width / 2) - 120, 30, Math.trunc(this.width / 2) + 120, 180, 0x90050500, 0xC0303060)
         if (block >= 0) {
-            let x = Math.trunc(this.width / 2) + var1 % 9 * 24 + -108;
-            let y = Math.trunc(this.height / 2) + Math.trunc(var1 / 9) * 24 + -60;
-            drawFadingBox(x - 3, y - 8, z + 23, y + 24 - 6, 0x90FFFFFF, 0xC0FFFFFF);
+            let x = Math.trunc(this.width / 2) + block % 9 * 24 + -108
+            let y = Math.trunc(this.height / 2) + Math.trunc(block / 9) * 24 + -60
+            BlockSelectScreen.fillGradient(x - 3, y - 8, z + 23, y + 24 - 6, 0x90FFFFFF, 0xC0FFFFFF)
         }
         BlockSelectScreen.textCentered(this.minecraft.font, "Select block", Math.trunc(this.width / 2), 40, 0xFFFFFF)
         let textures = this.minecraft.textures
