7f264d27682a728b390de862e6f8dde5fd219b55
Strultz <69481716+Strultz@users.noreply.github.com>
2023-06-16 09:44:14 -0500

    0.30 physic

--- src/com/mojang/minecraft/player/Player.ts
+++ src/com/mojang/minecraft/player/Player.ts
@@ -26,18 +26,18 @@ export class Player extends Entity {
                 this.resetPos()
             }
             if (jump && this.onGround) {
-                this.yd = 0.5
+                this.yd = 0.42
             }
             this.moveRelative(xa, ya, this.onGround ? 0.1 : 0.02)
         }
-        this.yd = this.yd - 0.08
+        this.yd -= 0.08
         this.move(this.xd, this.yd, this.zd)
         this.xd *= 0.91
         this.yd *= 0.98
         this.zd *= 0.91
         if (this.onGround) {
-            this.xd *= 0.7
-            this.zd *= 0.7
+            this.xd *= 0.6
+            this.zd *= 0.6
         }
     }
 
